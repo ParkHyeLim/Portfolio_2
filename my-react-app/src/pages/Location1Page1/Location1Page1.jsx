e08367e40103da7673e6781d7c253f734951267f
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2025-02-24 22:36:12 +0900

    feat: ar 모바일 환경에서도 동작되는지 확인차 psuh

--- my-react-app/src/pages/Location1Page1/Location1Page1.jsx
+++ my-react-app/src/pages/Location1Page1/Location1Page1.jsx
@@ -3,17 +3,6 @@ import { useNavigate } from "react-router-dom";
 import styles from "./Location1Page1.module.scss";
 import backgroundImg from '../../assets/image/backgroundImage.jpg';
 
-const messages = [
-    "비밀을 밝히기로 선택했군.",
-    "하지만 보물을 찾기 위해선 먼저 과거의 기억을 되살려야 한다.",
-    "\u00A0",
-    "이곳, 배재학당.",
-    "조선에 근대 교육의 바람이 불던 시절, 나는 이곳에서 새로운 희망을 보았다.",
-    "\u00A0",
-    "자, 첫 번째 단서가 여기에 있다.",
-    "과거를 이해하려면, 먼저 이곳에서 찾을 것이 있다.",
-];
-
 const Location1Page1 = () => {
 
     useEffect(() => {
@@ -29,7 +18,7 @@ const Location1Page1 = () => {
     }, []);
 
     return (
-        <div>
+        <div className={styles.cameraConainer}>
             <a-scene embedded arjs>
                 {/* AR 마커가 감지되면 보이는 오브젝트 */}
                 <a-marker preset="hiro">
